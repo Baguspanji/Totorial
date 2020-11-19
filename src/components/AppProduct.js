e45f5248c31c6c0a3702cffa54716d58018e5390
Baguspanji <47809071+Baguspanji@users.noreply.github.com>
2020-11-20 00:09:16 +0700

    u

--- src/components/AppProduct.js
+++ src/components/AppProduct.js
@@ -1,60 +1,64 @@
-import React from 'react';
-import { Text, View, StyleSheet, Image, TouchableOpacity, Alert  } from 'react-native';
+import React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 
+const onPress = () => {
+  Alert.alert("Alert Title", "My Alert Msg");
+  console.log('Klik!!');
+};
 
 const AppProduct = ({ product }) => {
-
   return (
-    <TouchableOpacity onPress={() =>
-        Alert.alert
-          (
-            "Alert Title",
-            "My Alert Msg",
-          )
-        }>
-        <View style={style.card}>
-            <Image style={style.img} source={{uri: product.image}}/>
-            <Text style={style.tittle}>{product.tittle}</Text>
-            <View style={style.row}>
-                <Text style={style.description}>{product.description}</Text>
-                <Text style={style.price}>Rp {product.price}</Text>
-            </View>
+    <TouchableOpacity onPress={() => onPress()}>
+      <View style={style.card}>
+        <Image style={style.img} source={{ uri: product.image }} />
+        <Text style={style.tittle}>{product.tittle}</Text>
+        <View style={style.row}>
+          <Text style={style.description}>{product.description}</Text>
+          <Text style={style.price}>Rp {product.price}</Text>
         </View>
+      </View>
     </TouchableOpacity>
   );
-}
+};
 
 const style = StyleSheet.create({
-    card: {
-        padding: 20,
-        marginVertical: 10,
-        marginHorizontal: 20,
-        borderRadius : 20,
-        backgroundColor: '#ffffff'
-
-    },
-    img: {
-        height: 250,
-    },
-    row: { 
-        flexDirection: 'row',
-        justifyContent: "space-between",
-        borderTopWidth: 2,
-    },
-    tittle: {
-        fontSize : 30,
-        color : '#004080',
-        fontWeight : 'bold',
-    },
-    description: {
-        fontSize : 20,
-        color: '#ff8000'
-    },
-    price: {
-        fontSize : 20,
-        color: '#ff0000',
-    }
+  card: {
+    padding: 20,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderRadius: 20,
+    backgroundColor: "#ffffff",
+  },
+  img: {
+    height: 250,
+    borderRadius: 20
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderTopWidth: 2,
+  },
+  tittle: {
+    fontSize: 30,
+    color: "#004080",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  description: {
+    fontSize: 20,
+    color: "#ff8000",
+  },
+  price: {
+    fontSize: 20,
+    color: "#ff0000",
+  },
 });
-
 
 export default AppProduct;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,17 +2,16 @@ import React from 'react';
 import { View, StyleSheet, SafeAreaView, Text, TouchableOpacity, Image } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
-    console.log(navigation);
     return (
       <SafeAreaView style={style.container}>
         <View style={style.col} >
           <TouchableOpacity onPress={() => navigation.navigate('Interractive')}>
-              <Text style={style.huruf} >InterractiveScreen</Text>
+              <Text style={style.huruf} >Interractive Screen</Text>
           </TouchableOpacity>
         </View>
         <View>
           <TouchableOpacity onPress={() => navigation.navigate('Santri')}>
-              <Image style={style.img} source={{uri : 'https://scontent.fsub9-1.fna.fbcdn.net/v/t1.0-9/p960x960/40901948_2154995611434081_5105080363764416512_o.jpg?_nc_cat=111&_nc_sid=85a577&_nc_ohc=VRFNCRgrU4EAX_bfT1D&_nc_ht=scontent.fsub9-1.fna&tp=6&oh=e0856e59617c10ed96d9532731142385&oe=5FB14368'}}/>
+              <Image style={style.img} source={{uri : 'https://i.pinimg.com/originals/69/6b/8b/696b8b8aa775971c56533fca526c879f.png'}}/>
           </TouchableOpacity>
         </View>
         <View style={style.col}>
